95330ae4252a90455a8f67d47dcb133d63d942a0
spacecoco <960176125@qq.com>
2018-11-28 16:53:18 +0800

    第一次

---
+++ dstest/src/main/java/com/atguigu/gmall/dstest/DstestApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.dstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DstestApplication.class, args);
+	}
+}
---
+++ sk/src/main/java/com/atguigu/gmalls/sk/SkApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmalls.sk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkApplication.class, args);
+	}
+}
